e6e5f5804600062f80ab2a1b4892b5f5fb13d40e
张小奕 <zxyqwe_2004@126.com>
2017-06-30 10:37:39 +0800

    ori

--- app/src/main/java/com/antlab/panotest/activity/MainActivity.java
+++ app/src/main/java/com/antlab/panotest/activity/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.WindowManager;
 import android.content.pm.ActivityInfo;
+import android.widget.TextView;
 
 import com.antlab.panotest.R;
 import com.antlab.panotest.render.PanoImageView;
@@ -14,6 +15,7 @@ import com.antlab.panotest.render.PanoVideoView;
 
 public class MainActivity extends AppCompatActivity implements PanoView.showOrientation {
     private PanoView m_panoview;
+    private TextView m_ori;
     private static final String TAG = MainActivity.class.getSimpleName();
     public static final int ImageView = 1;
     public static final int VideoView = 2;
@@ -29,6 +31,7 @@ public class MainActivity extends AppCompatActivity implements PanoView.showOrie
         WindowManager.LayoutParams params = getWindow().getAttributes();
         params.flags |= WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON;
         getWindow().setAttributes(params);
+        m_ori = (TextView) findViewById(R.id.text_ori);
     }
 
     private void init() {
@@ -38,10 +41,10 @@ public class MainActivity extends AppCompatActivity implements PanoView.showOrie
         switch (type) {
             default:
             case ImageView:
-                m_panoview = PanoImageView.build().setGLSurface(glSurfaceView).init(MainActivity.this);
+                m_panoview = PanoImageView.build().setGLSurface(glSurfaceView).init(MainActivity.this, MainActivity.this);
                 break;
             case VideoView:
-                m_panoview = PanoVideoView.build().setGLSurface(glSurfaceView).init(MainActivity.this);
+                m_panoview = PanoVideoView.build().setGLSurface(glSurfaceView).init(MainActivity.this, MainActivity.this);
                 break;
 
         }
@@ -68,5 +71,6 @@ public class MainActivity extends AppCompatActivity implements PanoView.showOrie
 
     @Override
     public void OnOrientationChanged(float[] orientation) {
+        m_ori.setText("" + orientation[0] + "\t" + orientation[1] + "\t" + orientation[2]);
     }
 }
