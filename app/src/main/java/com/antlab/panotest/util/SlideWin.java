ba46462e55845b961bf763425bd5cbdec97f65ce
张小奕 <zxyqwe_2004@126.com>
2017-06-30 17:32:44 +0800

    0

--- app/src/main/java/com/antlab/panotest/util/SlideWin.java
+++ app/src/main/java/com/antlab/panotest/util/SlideWin.java
@@ -10,23 +10,42 @@ public class SlideWin {
     private static final DecimalFormat dFor = new DecimalFormat("0.0");
 
     public SlideWin(int size) {
+        init(size);
+    }
+
+    private void init(int size) {
         m_size = size;
         m_data = new float[size];
         m_pos = 0;
+
     }
 
     public void add(float d) {
+        float ave = ave_int();
+        if (ave != 0 && Math.abs(d - ave) > Math.PI / 180 * 15) {
+            init(m_size);
+        }
         m_data[m_pos] = d;
         m_pos++;
         m_pos %= m_size;
     }
 
     public String average() {
+        return angleFormat(ave_int());
+    }
+
+    private float ave_int() {
         float sum = 0;
+        int tmp_size = m_size;
         for (float i : m_data) {
-            sum += i;
+            if (i != 0)
+                sum += i;
+            else
+                tmp_size--;
         }
-        return angleFormat(sum / m_size);
+        if (tmp_size == 0)
+            return 0;
+        return sum / tmp_size;
     }
 
     private String angleFormat(float angle) {
